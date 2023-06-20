b7f3f4d66d15f7c76fd6c6bd9ef589457fe14772
godgold11@gmail.com <godgold11@gmail.com>
2023-06-20 04:00:27 +0300

    Add project files.

---
+++ CodeChallenge01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CodeChallenge01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6 };
+            int[] reversedArray = ReverseArray(array);
+            Console.WriteLine("Reversed Array: " + string.Join(", ", reversedArray));
+        }
+
+        static int[] ReverseArray(int[] array)
+        {
+            int[] reversedArray = new int[array.Length];
+            int index = 0;
+
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                reversedArray[index] = array[i];
+                index++;
+            }
+
+            return reversedArray;
+        }
+    }
+}
