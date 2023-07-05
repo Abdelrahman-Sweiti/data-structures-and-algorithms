951bae8073a1e05aba5606b5a93c6396931c43f7
Abdelrahman Sweiti <102755704+Abdelrahman-Sweiti@users.noreply.github.com>
2023-07-05 15:17:26 +0300

    Update UnitTest1.cs

--- CodeChallenge06/TestProject1/UnitTest1.cs
+++ CodeChallenge06/TestProject1/UnitTest1.cs
@@ -1,5 +1,8 @@
 using System;
 using Xunit;
+using CodeChallenge06;
+using System.Collections.Generic;
+using static CodeChallenge06.Program;
 
 namespace TestProject1
 {
@@ -9,7 +12,7 @@ namespace TestProject1
         public void CanInstantiateEmptyLinkedList()
         {
             LinkedList list = new LinkedList();
-            Assert.Null(list.Head);
+            Assert.Null(list.head);
         }
 
         [Fact]
@@ -17,7 +20,7 @@ namespace TestProject1
         {
             LinkedList list = new LinkedList();
             list.Insert(3);
-            Assert.Equal(3, list.Head.Value);
+            Assert.Equal(3, list.head.Value);
         }
 
         [Fact]
@@ -26,7 +29,7 @@ namespace TestProject1
             LinkedList list = new LinkedList();
             list.Insert(2);
             list.Insert(1);
-            Assert.Equal(1, list.Head.Value);
+            Assert.Equal(1, list.head.Value);
         }
 
         [Fact]
@@ -36,7 +39,7 @@ namespace TestProject1
             list.Insert(3);
             list.Insert(2);
             list.Insert(1);
-            Assert.Equal("1 -> 2 -> 3 -> NULL", list.GetAllValues());
+            Assert.Equal("1 -> 2 -> 3 -> NULL", list.ToString());
         }
 
         [Fact]
@@ -66,7 +69,7 @@ namespace TestProject1
             list.Insert(3);
             list.Insert(2);
             list.Insert(1);
-            Assert.Equal("1 -> 2 -> 3 -> NULL", list.GetAllValues());
+            Assert.Equal("1 -> 2 -> 3 -> NULL", list.ToString());
         }
 
         [Fact]
@@ -75,7 +78,7 @@ namespace TestProject1
             LinkedList list = new LinkedList();
             list.Insert(1);
             list.Append(2);
-            Assert.Equal("1 -> 2 -> NULL", list.GetAllValues());
+            Assert.Equal("1 -> 2 -> NULL", list.ToString());
         }
 
         [Fact]
@@ -85,7 +88,7 @@ namespace TestProject1
             list.Insert(1);
             list.Append(2);
             list.Append(3);
-            Assert.Equal("1 -> 2 -> 3 -> NULL", list.GetAllValues());
+            Assert.Equal("1 -> 2 -> 3 -> NULL", list.ToString());
         }
 
         [Fact]
@@ -95,150 +98,10 @@ namespace TestProject1
             list.Insert(1);
             list.Insert(3);
             list.InsertBefore(3, 2);
-            Assert.Equal("1 -> 2 -> 3 -> NULL", list.GetAllValues());
+            Assert.Equal("1 -> 2 -> 3 -> NULL", list.ToString());
         }
 
         [Fact]
         public void CanInsertNodeBeforeFirstNodeOfLinkedList()
         {
             LinkedList list = new LinkedList();
-            list.Insert(2);
-            list.InsertBefore(2, 1);
-            Assert.Equal("1 -> 2 -> NULL", list.GetAllValues());
-        }
-
-        [Fact]
-        public void CanInsertNodeAfterMiddleNodeOfLinkedList()
-        {
-            LinkedList list = new LinkedList();
-            list.Insert(1);
-            list.Insert(3);
-            list.InsertAfter(1, 2);
-            Assert.Equal("1 -> 2 -> 3 -> NULL", list.GetAllValues());
-        }
-
-        [Fact]
-        public void CanInsertNodeAfterLastNodeOfLinkedList()
-        {
-            LinkedList list = new LinkedList();
-            list.Insert(1);
-            list.Insert(2);
-            list.InsertAfter(2, 3);
-            Assert.Equal("1 -> 2 -> 3 -> NULL", list.GetAllValues());
-        }
-
-        public class LinkedList
-        {
-            public Node Head { get; set; }
-
-            public LinkedList()
-            {
-                Head = null;
-            }
-
-            public void Insert(int value)
-            {
-                Node newNode = new Node(value);
-                newNode.Next = Head;
-                Head = newNode;
-            }
-
-            public bool Includes(int value)
-            {
-                Node current = Head;
-                while (current != null)
-                {
-                    if (current.Value == value)
-                        return true;
-                    current = current.Next;
-                }
-                return false;
-            }
-
-            public string GetAllValues()
-            {
-                Node current = Head;
-                string result = "";
-                while (current != null)
-                {
-                    result += $"{current.Value} -> ";
-                    current = current.Next;
-                }
-                result += "NULL";
-                return result;
-            }
-
-            public void Append(int value)
-            {
-                Node newNode = new Node(value);
-                if (Head == null)
-                {
-                    Head = newNode;
-                    return;
-                }
-
-                Node current = Head;
-                while (current.Next != null)
-                {
-                    current = current.Next;
-                }
-                current.Next = newNode;
-            }
-
-            public void InsertBefore(int targetValue, int newValue)
-            {
-                if (Head == null)
-                    return;
-
-                if (Head.Value == targetValue)
-                {
-                    Insert(newValue);
-                    return;
-                }
-
-                Node current = Head;
-                while (current.Next != null)
-                {
-                    if (current.Next.Value == targetValue)
-                    {
-                        Node newNode = new Node(newValue);
-                        newNode.Next = current.Next;
-                        current.Next = newNode;
-                        return;
-                    }
-                    current = current.Next;
-                }
-            }
-
-            public void InsertAfter(int targetValue, int newValue)
-            {
-                Node current = Head;
-                while (current != null)
-                {
-                    if (current.Value == targetValue)
-                    {
-                        Node newNode = new Node(newValue);
-                        newNode.Next = current.Next;
-                        current.Next = newNode;
-                        return;
-                    }
-                    current = current.Next;
-                }
-            }
-
-
-            //it wont read the Node class from the main so i pasted it here for testing
-            public class Node
-            {
-                public int Value { get; set; }
-                public Node Next { get; set; }
-
-                public Node(int value)
-                {
-                    Value = value;
-                    Next = null;
-                }
-            }
-        }
-    }
-}
