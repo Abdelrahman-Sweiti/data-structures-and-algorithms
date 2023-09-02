2edc47d2f1326fcd26ac4ef717711f4d8ce59387
Abdelrahman Sweiti <102755704+Abdelrahman-Sweiti@users.noreply.github.com>
2023-09-02 18:52:33 +0300

    cc27

---
+++ CodeChallenge27/CodeChallenge27/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace CodeChallenge27
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static void Sort(int[] arr)
+        {
+            int n = arr.Length;
+            if (n > 1)
+            {
+                int mid = n / 2;
+                int[] left = new int[mid];
+                int[] right = new int[n - mid];
+
+                Array.Copy(arr, 0, left, 0, mid);
+                Array.Copy(arr, mid, right, 0, n - mid);
+
+                Sort(left);
+                Sort(right);
+
+                Merge(left, right, arr);
+            }
+        }
+
+        public static void Merge(int[] left, int[] right, int[] arr)
+        {
+            int i = 0, j = 0, k = 0;
+
+            while (i < left.Length && j < right.Length)
+            {
+                if (left[i] <= right[j])
+                {
+                    arr[k] = left[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = right[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (i < left.Length)
+            {
+                arr[k] = left[i];
+                i++;
+                k++;
+            }
+
+            while (j < right.Length)
+            {
+                arr[k] = right[j];
+                j++;
+                k++;
+            }
+        }
+    }
+}
