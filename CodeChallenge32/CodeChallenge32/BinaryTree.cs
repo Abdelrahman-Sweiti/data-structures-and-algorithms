cf626a18e8c3af880a658e2c8d023c68d8a556ed
Abdelrahman Sweiti <102755704+Abdelrahman-Sweiti@users.noreply.github.com>
2023-10-02 07:48:44 +0300

    CC32

---
+++ CodeChallenge32/CodeChallenge32/BinaryTree.cs
@@ -0,0 +1,15 @@
+﻿using CodeChallenge32;
+using System;
+
+namespace CodeChallenge32
+{
+    public class BinaryTree
+    {
+        public Node root { get; set; }
+
+        public BinaryTree()
+        {
+            root = null;
+        }
+    }
+}
---
+++ CodeChallenge32/CodeChallenge32/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CodeChallenge32
+{
+    public class Node
+    {
+        public Node left { get; set; }
+        public Node right { get; set; }
+        public int value { get; set; }
+
+        public Node(int value)
+        {
+            this.value = value;
+            left = right = null;
+        }
+    }
+}
