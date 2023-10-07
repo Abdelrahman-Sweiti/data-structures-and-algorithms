873ddfccbc00d9906b5dd0fdfe0d5933e649f202
Abdelrahman Sweiti <102755704+Abdelrahman-Sweiti@users.noreply.github.com>
2023-10-07 21:40:59 +0300

    new

---
+++ CodeChallenge36/CodeChallenge36/Vertex.cs
@@ -0,0 +1,23 @@
+﻿using CodeChallenge36;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge36
+{
+    public class Vertex
+    {
+        public Object Value { get; set; }
+        public List<Edge> Edge { get; set; } = new List<Edge>();
+        public bool Visited { get; set; }
+
+        public Vertex(Object value)
+        {
+            Value = value;
+            Visited = false;
+        }
+
+    }
+}
