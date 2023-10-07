4f9dda67bcf87825bf39e898b40025cdfb1ef147
Abdelrahman Sweiti <102755704+Abdelrahman-Sweiti@users.noreply.github.com>
2023-10-07 21:49:25 +0300

    new

---
+++ CodeChallenge38/CodeChallenge38/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeChallenge38
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
